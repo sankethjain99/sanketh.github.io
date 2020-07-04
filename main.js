ee74467011baf4c08f7d3e1627eb4a58f2b77b7e
sanketh <jsanketh1799@gmail.com>
2020-07-04 17:37:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCLrgznTPnfac7_PYzo5otljDSuz7qUli0",
+    authDomain: "myprofile-9eadb.firebaseapp.com",
+    databaseURL: "https://myprofile-9eadb.firebaseio.com",
+    projectId: "myprofile-9eadb",
+    storageBucket: "myprofile-9eadb.appspot.com",
+    messagingSenderId: "712574481816",
+    appId: "1:712574481816:web:642509f6d12fcb1d71a7ed",
+    measurementId: "G-LT0HSRPT6Q"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
